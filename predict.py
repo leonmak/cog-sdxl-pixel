7356e66eba5445cd09424b5fa294ef0fcdd3904d
Leon Mak <leonmakansheng@gmail.com>
2024-03-10 13:05:29 +0800

    load

--- predict.py
+++ predict.py
@@ -73,12 +73,12 @@ def download_weights(url, dest):
 class Predictor(BasePredictor):
     def load_trained_weights(self, weights_url, pipe):
         FILENAME = "pixel-art-xl.safetensors"
-        print(os.listdir('.'))
         has_file = os.path.exists(os.path.join(os.curdir, FILENAME))
         if not has_file:
             weights_tar_data = requests.get(weights_url).content
             with tarfile.open(fileobj=BytesIO(weights_tar_data), mode='r') as tar_ref:
                 tar_ref.extractall()
+        print(os.listdir('.'))
 
         # weights can be a URLPath, which behaves in unexpected ways
         pipe.load_lora_weights(FILENAME, adapter_name="pixel")
